925755927082a9fb3420093a18cecca1e40b0105
saadchow <109829705+saadchow@users.noreply.github.com>
2023-09-29 03:35:19 -0400

    update server.js

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ mongoose.connect(mongoURI, {
 .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 // Middleware
-// server.js
 app.use(cors()); // replace with the URL of your client
 app.use(express.json());
 
@@ -28,9 +27,6 @@ app.use('/api', animeRoutes);
 // Serve static files from the React app
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
 }
 
 // Error handling middleware
@@ -39,10 +35,14 @@ app.use(function(err, req, res, next) {
   res.status(500).send('Something broke!');
 });
 
+// Catch-all handler
+if (process.env.NODE_ENV === "production") {
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
-
-
