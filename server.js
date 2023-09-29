30cd60d2822f768b9a37c2a8d1714f42b820c425
saadchow <109829705+saadchow@users.noreply.github.com>
2023-09-29 03:18:35 -0400

    revert to working version

--- server.js
+++ server.js
@@ -26,11 +26,12 @@ const animeRoutes = require('./routes/api');
 app.use('/api', animeRoutes);
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
+}
 
 // Error handling middleware
 app.use(function(err, req, res, next) {
