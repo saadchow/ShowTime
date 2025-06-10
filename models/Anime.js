0869cf58601fa39c7d9de4e6dca9949a7331fd1e
Saad Chowdhury <109829705+saadchow@users.noreply.github.com>
2025-06-10 00:03:45 -0400

    Update Anime.js

--- models/Anime.js
+++ models/Anime.js
@@ -16,23 +16,3 @@ const animeSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Anime', animeSchema);
-=======
-// models/Anime.js
-const mongoose = require('mongoose');
-
-const animeSchema = new mongoose.Schema({
-  user_id: String, // Clerk user id
-  mal_id: Number,
-  title: String,
-  synopsis: String,
-  images: Object,
-  rank: Number,
-  score: Number,
-  status: String,
-  rating: String,
-  list: String, // 'currently watching', 'plan to watch', 'completed'
-  episodes: Number,
-  type: String,
-});
-
-module.exports = mongoose.model('Anime', animeSchema);
