2ea73dc7ab67d45deb677f0c7fb5f0ee9719e1e6
Saad Chowdhury <109829705+saadchow@users.noreply.github.com>
2025-06-10 00:15:13 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const mongoURI = process.env.MONGODB_URI;
+const mongoURI = process.env.REACT_APP_MONGODB_URI;
 
 // Connect to MongoDB
 mongoose.connect(mongoURI, {
@@ -17,7 +17,7 @@ mongoose.connect(mongoURI, {
 .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 // Middleware
-app.use(cors());
+app.use(cors()); // replace with the URL of your client
 app.use(express.json());
 
 // Routes
@@ -27,11 +27,6 @@ app.use('/api', animeRoutes);
 // Serve static files from the React app
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-
-  // Catch-all handler
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
 }
 
 // Error handling middleware
@@ -39,6 +34,13 @@ app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
+
+// Catch-all handler
+if (process.env.NODE_ENV === "production") {
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Start the server
 app.listen(port, () => {
