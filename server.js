47b3ad7b73d772039e74b4ac325ada94bdaa893b
Saad Chowdhury <109829705+saadchow@users.noreply.github.com>
2025-06-09 23:16:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(mongoURI, {
 .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 // Middleware
-app.use(cors()); // replace with the URL of your client
+app.use(cors());
 app.use(express.json());
 
 // Routes
@@ -27,6 +27,11 @@ app.use('/api', animeRoutes);
 // Serve static files from the React app
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  // Catch-all handler
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // Error handling middleware
@@ -34,62 +39,6 @@ app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
-
-// Catch-all handler
-if (process.env.NODE_ENV === "production") {
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
-=======
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
-require('dotenv').config();
-
-const app = express();
-const port = process.env.PORT || 5000;
-const mongoURI = process.env.REACT_APP_MONGODB_URI;
-
-// Connect to MongoDB
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.error('Error connecting to MongoDB:', err));
-
-// Middleware
-app.use(cors()); // replace with the URL of your client
-app.use(express.json());
-
-// Routes
-const animeRoutes = require('./routes/api');
-app.use('/api', animeRoutes);
-
-// Serve static files from the React app
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-// Error handling middleware
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
-// Catch-all handler
-if (process.env.NODE_ENV === "production") {
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 // Start the server
 app.listen(port, () => {
