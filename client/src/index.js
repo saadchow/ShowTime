12c87843e774da8633df45310b8443ed49063942
Saad Chowdhury <109829705+saadchow@users.noreply.github.com>
2025-08-08 17:45:22 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './GlobalStyle';
 import { GlobalContextProvider } from './context/global';
 
@@ -17,4 +16,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
